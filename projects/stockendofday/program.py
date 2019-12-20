e3d415f4f70d227c01a321606cbba1133cc7d841
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2019-12-20 14:54:19 +0100

    Update program.py

--- projects/stockendofday/program.py
+++ projects/stockendofday/program.py
@@ -5,5 +5,6 @@ from bs4 import BeautifulSoup
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.56786"}
 
-dataark = pd.read_excel("p-e.xlsm") # læser datasæt.
+dataark = pd.read_excel("ADRESSES.xlsm") # læser datasæt.
 webadresser = dataark.adresser # laver en variabel med adresser.
+
