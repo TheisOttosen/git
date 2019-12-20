b241f99846f6fff2728c24b6ee6d18365b7788c3
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2019-12-20 14:46:47 +0100

    Update program.py

--- projects/stockendofday/program.py
+++ projects/stockendofday/program.py
@@ -1,5 +1,6 @@
 #importspp0
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.56786"}
