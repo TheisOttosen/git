1689fdfed8ceb70b2ca8e3b902c9ed394a9dd743
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2019-12-20 14:58:25 +0100

    Update program.py

--- projects/stockendofday/program.py
+++ projects/stockendofday/program.py
@@ -1,10 +1,13 @@
-#importspp0
+# imports.
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+# importerer user-agent.
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.56786"}
 
+# importere adresser.
 dataark = pd.read_excel("ADRESSES.xlsm") # læser datasæt.
 webadresser = dataark.adresser # laver en variabel med adresser.
+print (webadresser)
 
