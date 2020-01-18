6e62f1a1902f25fea74eb3e470087d8b7472660d
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2020-01-18 20:29:51 +0100

    g

--- python/django/mysite/main/views.py
+++ python/django/mysite/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("Theis")
+    return HttpResponse("<h1>Theis</h1> <h2>index</h2>")
+
+def v1(response):
+    return HttpResponse("<h1>Theis</h1> <h2>view 1</h2>")
