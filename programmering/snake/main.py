ef9450f4a8a56fbb96c1b43181998744d29290b3
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2020-03-02 17:41:18 +0100

    snake

--- programmering/snake/main.py
+++ programmering/snake/main.py
@@ -1,22 +1,67 @@
-import pygame
-pygame.init()
+import time # For FPS
+import pygame # Pygame import
+pygame.init() # Pygame init
 
-window = pygame.display.set_mode((1000,1000))
-pygame.display.set_caption("Snake")
 
-# Spillerens variabler
+
+window = pygame.display.set_mode((1000,1000)) # Makes the window
+pygame.display.set_caption("Snake") # Changes the caption of the game
+
+
+
+# Players variables
 posX = 50
 posY = 50
 size = 50
 length = 0
+PlayerColor = (255,0,0)
 
+
+
+# Game variables
+BackGroundColor = (0,0,0)
+FPS = 30
+
+
+
+# Draws everything
+def DrawContent():
+    pygame.draw.rect(window, BackGroundColor, (0, 0, 1000, 1000)) # Background
+    pygame.draw.rect(window, PlayerColor, (posX, posY, size, size)) # Player
+    pygame.display.update()
+    pass
+
+
+
+# Makes the player move
+def Movement():
+    for event in pygame.event.get():
+        if event.type == pygame.K_w:
+            posY -= 1
+        if event.type == pygame.K_a:
+            posY -= 1
+        if event.type == pygame.K_s:
+            posY -= 1
+        if event.type == pygame.K_d:
+            posY -= 1
+    pass
+
+
+
+# Main loop
 run = True
-
 while run:
-    pygame.time.delay(100)
+    time.sleep(1/FPS)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    Movement()
+    DrawContent()
+
+    
+
+
 
 pygame.quit()
