194ab791dddb389a41b2f9ec209823af4ebb686e
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2020-04-14 20:48:21 +0200

    configFetch

--- programmering/configFetch.py
+++ programmering/configFetch.py
@@ -1,11 +1,17 @@
 from bs4 import BeautifulSoup as BS4
 import requests
 
-URL = "http://esafslappet.ml/pythoncfg.cfg"
+URL = "https://pastebin.com/y8aXEmuc"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
 page = requests.get(URL, headers=headers)
 soup = BS4(page.content, "html.parser")
 
-cfg = soup.find({"id" : "paste_code"})
+cfg = soup.find(id="paste_code").get_text()
 
 print(cfg)
+
+text_file = open("G:\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\sger", "w")
+n = text_file.write(cfg)
+text_file.close()
+
+
