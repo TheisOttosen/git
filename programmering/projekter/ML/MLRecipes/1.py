acb0548671138d9da8f911b15c9d3e75929a4335
TheisOttosen <52250167+TheisOttosen@users.noreply.github.com>
2020-05-06 15:37:11 +0200

    .

---
+++ programmering/projekter/ML/MLRecipes/1.py
@@ -0,0 +1,15 @@
+import sklearn
+
+# Colors as numbers
+ColorRed = 0
+ColorOrange = 1
+ColorYellow = 2
+ColorGreen = 3
+
+# Textures as numbers
+TextBumpy = 0
+TextSmooth = 1
+TextHairy = 2
+
+features = []
+labels = []
